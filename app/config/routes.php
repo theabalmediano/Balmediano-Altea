f3fa5f3ce8047f1cc0eb8622df41058854ea4f07
theabalmediano <theabalmediano585@gmail.com>
2025-09-19 23:21:37 +0800

    comment

--- app/config/routes.php
+++ app/config/routes.php
@@ -46,9 +46,5 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 $router->get('/', 'UsersController::index');
 $router->match('/users/create', 'UsersController::create', ['GET', 'POST']);
 $router->match('/users/update/{id}', 'UsersController::update', ['GET', 'POST']);
-$router->match('/users/delete/{id}', 'UsersController::delete', ['POST']);
-$router->match('/users/soft_delete/{id}', 'UsersController::soft_delete', ['POST']);
-$router->get('/users/restore', 'UsersController::restore');
-$router->match('/users/retrieve/{id}', 'UsersController::retrieve', ['POST']);
-
+$router->get('/users/delete/{id}', 'UsersController::delete');
  

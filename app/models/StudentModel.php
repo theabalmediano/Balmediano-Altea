b1514edba9c1ec019e637fff2818bff1d2f366ff
theabalmediano <theabalmediano585@gmail.com>
2025-09-30 00:44:45 +0800

    comment

---
+++ app/models/StudentModel.php
@@ -0,0 +1,18 @@
+<?php
+class StudentModel extends Model
+{
+    protected $table = 'users';
+
+    public function get_user_by_id($id)
+    {
+        return $this->db->table($this->table)
+                        ->where('id', $id)
+                        ->get();
+    }
+
+    public function get_all_users()
+    {
+        return $this->db->table($this->table)->get_all();
+    }
+}
+?>

e4288f80005cb0abe4ed7b464f8c435fabb1c790
theabalmediano <theabalmediano585@gmail.com>
2025-09-30 17:10:18 +0800

    try

--- app/config/routes.php
+++ app/config/routes.php
@@ -45,7 +45,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $router->match('/', 'AuthController::register', ['GET','POST']);
 
-$router->get('/', 'UsersController::index');
+$router->get('/users', 'UsersController::index');
 $router->match('/users/create', 'UsersController::create', ['GET', 'POST']);
 $router->match('/users/update/{id}', 'UsersController::update', ['GET', 'POST']);
 $router->get('/users/delete/{id}', 'UsersController::delete');
